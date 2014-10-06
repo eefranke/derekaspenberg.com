7435c9b1e22852e50ec549644c947912f6ff28ef
Erin <whelp@whelps-MacBook-Air.local>
2014-10-06 11:51:51 -0400

    October 2014
    October 2014 single page website to replace previous website.
    Background image behind about section is static; will be swapped for
    curated images that change on page load.  Needs .ico and better
    handling of Vimeo cache.  Possible adjustments of section ratios needed.

---
+++ index.php
@@ -0,0 +1,49 @@
+                                <!DOCTYPE html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Derek Aspenberg</title>
+        <meta name="description" content="Derek Aspenberg, Director of Photography">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link href='http://fonts.googleapis.com/css?family=Lusitana' rel='stylesheet' type='text/css'>
+<link href='/style.css' rel='stylesheet' type='text/css'>
+
+<style>
+
+/* iPad Settings */
+
+@media only screen 
+and (min-width : 481px) 
+and (max-width : 1024px) {
+.menu {display: none !important;}
+.menuipad {display: block !important;}
+.menumobile {display: none;}
+}
+</style>
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="videoWrapper"><iframe src="http://player.vimeo.com/video/74493856" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+<div class="menu">Derek Aspenberg, <span style="font-size: .8em;">Director of Photography</span><a href="http://vimeo.com/user3928447" target="_blank"><span class="vimeo"></span></a><a href="http://instagram.com/d_rock_attack" target="_blank"><span class="instagram"></span></a><a href="#about"><span class="contact"></span></a></div>
+
+<div class="menuipad">Derek Aspenberg<br /><span style="font-size: .8em;">Director of Photography</span>
+<a href="http://vimeo.com/user3928447" target="_blank"><span class="vimeo"></span></a><a href="http://instagram.com/d_rock_attack" target="_blank"><span class="instagram"></span></a><a href="#about"><span class="contact"></span></a></div>
+
+<div class="menumobile">Derek Aspenberg<br /><span style="font-size: .8em;">Director of Photography</span><br /><br />
+<a href="http://vimeo.com/user3928447" target="_blank"><span class="vimeo"></span></a><a href="http://instagram.com/d_rock_attack"><span class="instagram" target="_blank"></span></a><a href="#about"><span class="contact"></span></a></div>
+
+
+<div id="about"><div class="abouthold"><p>Derek was introduced to the world of art through music. Playing classical guitar taught the importance of practice and technique. Composing for chamber orchestra taught the satisfaction of creation.</p> 
+<p>Music was put on hold with the discovery of film and further with the discovery of light and composition.  The tools are different but the thought process has remained surprisingly similar.</p> 
+<p>Derek has shot for Grey advertising, LPZ media, Article 19 Films, Means of Production, Cove Creek Productions and more.</p> 
+<p>Derek likes movies with long dolly shots. </p><a href="http://vimeo.com/user3928447" target="_blank"><span class="vimeo"></span></a><a target="_blank" href="http://instagram.com/d_rock_attack"><span class="instagram"></span></a><a href="mailto:daspenberg@gmail.com"><span class="mail"></span></a><a href="#"><span class="reel"></span></a>
+</div></div>
+    </div>
+
+</body>
+</html>
+
+                            
